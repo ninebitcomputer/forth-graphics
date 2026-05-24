0dd06953f054c554b7e83ca12e10b8b031c5ef25
Daniel Jin <mrpickles123115@gmail.com>
2026-05-24 04:04:21 -0400

    circle follow

--- fih/main.fs
+++ fih/main.fs
@@ -5,6 +5,8 @@ require ./anim.fs
 800 constant swidth
 450 constant sheight
 
+5e fconstant follow-speed
+
 60 constant fps
 
 variable addr-head 67e 67e >Vector2 addr-head !
@@ -25,9 +27,29 @@ create tempv Vector2 allot
 
 	  GetMouseX s>f GetMouseY s>f tempv Vector2!
 
-	  head tempv RED DrawLineV
+	  tempv 10 BLUE DrawCircleV
 
-	  tempv Vector2@ head Vector2@ vec2- tempv Vector2!
+	  head Vector2@
+	  tempv Vector2@
+	  head Vector2@
+
+	  vec2-
+	  fover fover
+	  vec2-mag fdup 10e f< if
+		fdrop fdrop fdrop 0e 0e
+	  else
+		vec2s/ follow-speed vec2s*
+	  then
+
+	  vec2+
+
+	  tempv Vector2!
+
+	  head tempv GREEN DrawLineV
+
+	  tempv Vector2@ head Vector2!
+
+
 
 	  head 20 RED DrawCircleV
 
